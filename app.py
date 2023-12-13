e462db7196b9ec059a8ea1f011fe67549342f071
Admin <charliebthines@icloud.com>
2023-12-13 21:37:03 +0000

    2 COMPLETE

--- app.py
+++ app.py
@@ -15,6 +15,27 @@ db = SQLAlchemy(app)
 #application instance
 qrcode = QRcode(app)
 
+#Error handling
+@app.errorhandler(400)
+def bad_request_error(error):
+    return render_template('errors/400.html'), 400
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('errors/403.html'), 403
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html'), 500
+
+@app.errorhandler(503)
+def service_unavailable_error(error):
+    return render_template('errors/503.html'), 503
+
 
 # HOME PAGE VIEW
 @app.route('/')
